a09580cb0ddd1c8a8e6edaba41f0752cdceddfae
Nox-User <cilu5987@gmail.com>
2025-11-05 16:25:11 -0300

    Refactor card rendering and variable names
    Updated the card rendering logic and fixed variable names.

--- index.js
+++ index.js
@@ -51,17 +51,17 @@ const data = [
 
 document.getElementById("allCardsContainer").innerHTML = data
   .map(
-    (eachCard) =>
+    (functionCard) =>
     `
-      <div class="eachCard">
-        <div class="cardContent">
+      <div class="functionCard">
+        <div class="functioncardContent">
           <div class="cardImage">
-            <img src="${eachCard.logo}" alt="${eachCard.title}-logo">
+            <img src="${functionCard.logo}" alt="${functionCard.title}-logo">
           </div>
           <div class="cardInfo">
-            <h2 class="cardTitle">${eachCard.title}</h2>
-            <span class="cardSubTitle">${eachCard.subTitle}</span>
-            <p class="cardText">${eachCard.descprition}</p>
+            <h2 class="cardTitle">${functionCard.title}</h2>
+            <span class="cardSubTitle">${functionCard.subTitle}</span>
+            <p class="cardText">${functionCard.descprition}</p>
           </div>
         </div>
         <div class="userActions">
@@ -69,7 +69,7 @@ document.getElementById("allCardsContainer").innerHTML = data
           <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
           </svg> Guardar</button>
           <button class="btn-primary">
-          <a href="${eachCard.apiLink}" rel="noopener noreferrer">IR PARA</a> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <a href="${functionCard.apiLink}" rel="noopener noreferrer">IR PARA</a> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
           </svg> </button>
         </div>
@@ -77,3 +77,4 @@ document.getElementById("allCardsContainer").innerHTML = data
     `
   )
   .join("");
+
